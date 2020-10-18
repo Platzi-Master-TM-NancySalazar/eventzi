ee42822d66a9b34645cedc5a25efdfed30c87607
Jhon Angulo <jhonmanuelm89@gmail.com>
2020-10-18 11:41:52 -0500

    added home page

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Layout from '../components/Layout'
+import Home from '../pages/Home'
 import SignUp from '../pages/SignUp'
 import SignUp2 from '../pages/SignUp2'
 import Login from '../pages/Login'
@@ -11,7 +12,8 @@ const App = () => (
     <BrowserRouter>
         <Layout>
             <Switch>
-                <Route exact path='/' component={SignUp} />
+                <Route exact path='/' component={Home} />
+                <Route exact path='/singup' component={SignUp} />
                 <Route exact path='/signup2' component={SignUp2} />
                 <Route exact path='/login' component={Login} />
             </Switch>

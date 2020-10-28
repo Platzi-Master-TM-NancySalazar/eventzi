e214b039ee9fa64de0e6a51cbb83773c298b9f6e
Jhon Angulo <jhonmanuelm89@gmail.com>
2020-10-27 20:22:54 -0500

    added page admin

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Layout from '../components/Layout'
 import Home from '../pages/Home'
+import Admin from '../pages/Admin'
 import SignUp from '../pages/SignUp'
 import SignUp2 from '../pages/SignUp2'
 import Login from '../pages/Login'
@@ -14,6 +15,7 @@ const App = () => (
         <Layout>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/admin' component={Admin} />
                 <Route exact path='/singup' component={SignUp} />
                 <Route exact path='/signup2' component={SignUp2} />
                 <Route exact path='/login' component={Login} />

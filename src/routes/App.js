575efcaddf91beb6ea62791f42115893846ee767
Luis <luisglopez7777@gmail.com>
2020-11-01 22:06:25 -0600

    Ready to merge

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Layout from '../components/Layout'
 import Home from '../pages/Home'
 import SignUp from '../pages/SignUp'
-// import SignUp2 from '../pages/SignUp2'
 import Login from '../pages/Login'
 import Template1 from '../pages/Template1'
 import EventPanel from '../pages/EventPanel'
@@ -24,8 +23,6 @@ const App = () => {
           {isAuth ? <Route exact path='/' component={Home} /> : <Route exact path='/' component={Login} />}
           {isAuth ? <Route exact path='/eventPanel' component={EventPanel} /> : <Route exact path='/eventPanel' component={Login} />}
           <Route exact path='/signup' component={SignUp} />
-          {/* <Route exact path='/signup2' component={SignUp2} /> */}
-          <Route exact path='/login' component={Login} />
           <Route component={NotFound} />
 
         </Switch>

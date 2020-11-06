ca7f6c5b8d03a92a3249204cf203f50a8d5b18bf
Jhon Angulo <jhonmanuelm89@gmail.com>
2020-11-05 20:14:16 -0500

    analytic finished component

--- src/charts/BarChart.js
+++ src/charts/BarChart.js
@@ -1,34 +1,43 @@
-import React, { useEffect } from "react";
-import c3 from "c3";
+import React, { useEffect } from 'react'
+import c3 from 'c3'
 
 const BarChart = () => {
-
   useEffect(() => {
     c3.generate({
-      bindto: "#bar_chart",
-      size: {
-        height: 400,
-        width: 600
-      },
+      bindto: '#bar_chart',
+      // size: {
+      //   height: 400,
+      //   width: 400
+      // },
       data: {
         columns: [
           ['data1', 30, 200, 100, 400, 150, 250],
-          ['data2', 130, 100, 140, 200, 150, 50]
+          ['data2', 130, 100, 140, 200, 150, 50],
         ],
-        type: 'bar'
+        type: 'bar',
       },
       bar: {
         width: {
-          ratio: 0.5 // this makes bar width 50% of length between ticks
-        }
+          ratio: 0.5, // this makes bar width 50% of length between ticks
+        },
         // or
         //width: 100 // this makes bar width 100px
-      }
-    });
+      },
+      grid: {
+        focus: {
+          show: true,
+        },
+        x: {
+          show: false,
+        },
+        y: {
+          show: true,
+        },
+      },
+    })
   }, [])
 
-
-  return <div id="bar_chart" />;
-};
+  return <div id='bar_chart' className='analitics_chart' />
+}
 
 export default BarChart

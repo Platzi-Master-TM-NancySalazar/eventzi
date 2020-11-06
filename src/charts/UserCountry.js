21f006dda1754509924c3c414c29b48fc6534de5
JhonAngulo <jhonmanuelm89@gmail.com>
2020-11-06 16:52:28 -0500

    new charts

--- src/charts/UserCountry.js
+++ src/charts/UserCountry.js
@@ -1,42 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import c3 from 'c3'
+import Loader from '../components/Loader'
 
 const UserCountry = () => {
-    useEffect(() => {
-        c3.generate({
-            bindto: '#user_country',
-            // size: {
-            //   height: 400,
-            //   width: 400
-            // },
-            data: {
-                columns: [
-                    ['data1', 39, 37, 38, 43, 35, 45, 46, 30, 44, 48],
-                ],
-                type: 'bar',
-            },
-            bar: {
-                width: {
-                    ratio: 0.5, // this makes bar width 50% of length between ticks
-                },
-                // or
-                //width: 100 // this makes bar width 100px
-            },
-            grid: {
-                focus: {
-                    show: true,
-                },
-                x: {
-                    show: false,
-                },
-                y: {
-                    show: true,
-                },
-            },
-        })
-    }, [])
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
 
-    return <div id='user_country' className='analitics_chart' />
+  useEffect(() => {
+    if (!data.length) {
+      fetch('https://eventziapi.herokuapp.com/dashboard/users/country')
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data)
+          setLoading(false)
+        })
+    }
+  }, [data])
+
+  const generateChart = () => {
+    c3.generate({
+      bindto: '#user_country',
+      data: {
+        columns: [['Countries'].concat(data)],
+        type: 'bar',
+      },
+      bar: {
+        width: {
+          ratio: 0.5, 
+        },
+      },
+      grid: {
+        focus: {
+          show: true,
+        },
+        x: {
+          show: false,
+        },
+        y: {
+          show: true,
+        },
+      },
+    })
+  }
+
+  return (
+    <>
+      <div id='user_country' className='analitics_chart' />
+      {loading ? <Loader /> : generateChart()}
+    </>
+  )
 }
 
 export default UserCountry
--- src/charts/UserGender.js
+++ src/charts/UserGender.js
@@ -1,54 +1,41 @@
-import React, { useEffect } from 'react';
-import c3 from 'c3';
-import axios from "axios";
-
-function data(endpoint) {
-    axios({
-        method: 'GET',
-        url: endpoint
-    }).then(res => {
-        return (res.data)
-    }).catch(err =>
-        alert(err))
-}
-
-let gender = data('https://eventziapi.herokuapp.com/dashboard/MaleFemale/total')
+import React, { useEffect, useState } from 'react'
+import c3 from 'c3'
+import Loader from '../components/Loader'
 
 const UserGender = () => {
-    useEffect(() => {
-        c3.generate({
-            bindto: '#user_gender',
-            // size: {
-            //   height: 400,
-            //   width: 600
-            // },
-            data: {
-                // iris data from R
-                columns: [
-                    [
-                        'Female',
-                        50
-                    ],
-                    [
-                        'Male',
-                        50
-                    ],
-                ],
-                type: 'pie',
-                onclick: function (d, i) {
-                    console.log('onclick', d, i)
-                },
-                onmouseover: function (d, i) {
-                    console.log('onmouseover', d, i)
-                },
-                onmouseout: function (d, i) {
-                    console.log('onmouseout', d, i)
-                },
-            },
-        })
-    }, [])
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
 
-    return <div id='user_gender' />
+  const generateChart = () => {
+    c3.generate({
+      bindto: '#user_gender',
+      data: {
+        columns: [
+          ['Female', data[0]],
+          ['Male', data[1]],
+        ],
+        type: 'pie',
+      },
+    })
+  }
+
+  useEffect(() => {
+    if (!data.length) {
+      fetch('https://eventziapi.herokuapp.com/dashboard/MaleFemale/total')
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data)
+          setLoading(false)
+        })
+    }
+  }, [data])
+
+  return (
+    <>
+      <div id='user_gender' />
+      {loading ? <Loader /> : generateChart()}
+    </>
+  )
 }
 
 export default UserGender
